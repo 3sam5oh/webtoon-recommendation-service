fccd7c7b4e5f98f05311a6cb5697b2373e64fcad
jhlee <jun73045928@gmail.com>
2024-08-08 20:10:31 +0900

    refactor(openSearch) AWS OpenSearch Service 연동

--- webtoon-search-back/webtoon-search/src/main/java/com/samsamohoh/webtoonsearch/adapter/config/SearchEngineConfig.java
+++ webtoon-search-back/webtoon-search/src/main/java/com/samsamohoh/webtoonsearch/adapter/config/SearchEngineConfig.java
@@ -1,17 +1,22 @@
 package com.samsamohoh.webtoonsearch.adapter.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.opensearch.client.RestClient;
-import org.opensearch.client.json.jackson.JacksonJsonpMapper;
-import org.opensearch.client.opensearch.OpenSearchClient;
-import org.opensearch.client.transport.OpenSearchTransport;
-import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.opensearch.client.RestClientBuilder;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-public class SearchEngineConfig {
+@EnableElasticsearchRepositories
+public class SearchEngineConfig extends AbstractOpenSearchConfiguration {
 
     @Value("${spring.opensearch.uris}")
     private String opensearchUri;
@@ -22,20 +27,44 @@ public class SearchEngineConfig {
     @Value("${spring.opensearch.socket-timeout}")
     private int socketTimeout;
 
+    @Value("${spring.opensearch.id}")
+    private String id;
+
+    @Value("${spring.opensearch.pwd}")
+    private String pwd;
+
+    /*
+    * Local 환경에서 사용한 client
+    * */
+//    @Bean
+//    public OpenSearchClient openSearchClient() {
+//        RestClient restClient = RestClient.builder(HttpHost.create(opensearchUri))
+//                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+//                        .setConnectTimeout(connectionTimeout)
+//                        .setSocketTimeout(socketTimeout)
+//                )
+//                .build();
+//
+//        OpenSearchTransport transport = new RestClientTransport(
+//                restClient,
+//                new JacksonJsonpMapper()
+//        );
+//
+//        return new OpenSearchClient(transport);
+//    }
+
+    @Override
     @Bean
-    public OpenSearchClient openSearchClient() {
-        RestClient restClient = RestClient.builder(HttpHost.create(opensearchUri))
-                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
-                        .setConnectTimeout(connectionTimeout)
-                        .setSocketTimeout(socketTimeout)
-                )
-                .build();
+    public RestHighLevelClient opensearchClient() {
 
-        OpenSearchTransport transport = new RestClientTransport(
-                restClient,
-                new JacksonJsonpMapper()
-        );
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(id, pwd));
 
-        return new OpenSearchClient(transport);
+        RestClientBuilder builder = RestClient.builder(
+                        new HttpHost(opensearchUri, 443, "https"))
+                .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder
+                        .setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(builder);
     }
 }
